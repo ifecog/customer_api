dc2ad7c6f907e923c0c9f87d8ae83962d64ad172
progIfe <ifeoluwasamson90@gmail.com>
2023-01-02 12:34:31 +0100

    Initialization and Configuration of core app, Project setup of the Django REST Framework

---
+++ core/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Profession(models.Model):
+    description = models.CharField(max_length=50)
+
+
+class Datasheet(models.Model):
+    description = models.CharField(max_length=50)
+    historical_data = models.TextField()
+
+    def __str__(self):
+        return self.description
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    professions = models.ManyToManyField(Profession)
+    data_sheet = models.OneToOneField(Datasheet, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Document(models.Model):
+    PP = 'PP'
+    ID = 'ID'
+    OT = 'OT'
+
+    DOC_TYPES = (
+        (PP, 'Passport'),
+        (ID, 'Identity card'),
+        (OT, 'others')
+    )
+
+    dtype = models.CharField(choices=DOC_TYPES, max_length=2)
+    doc_number = models.CharField(max_length=50)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.doc_number
--- customer_api/settings.py
+++ customer_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,12 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core.apps.CoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
